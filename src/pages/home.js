72b438c368b990f7a733feeb8f94287fe3a047a1
Josh Akeman <joshakeman@gmail.com>
2019-08-14 22:40:21 -0400

    created dummy data to avoid frequent api calls while building

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,22 +13,45 @@ export class home extends Component {
 
     /* !!!!!!!!!!!!!!!!! WARNING !!!!!!!!!!!!!!!!! */
     /* Every reload is another DB call to Firebase! */
-        const baseUrl = 'https://us-central1-tokker.cloudfunctions.net/api'
-        axios.get('https://us-central1-tokker.cloudfunctions.net/api/toks')
-            .then(res => {
-                console.log(res.data)
-                this.setState({
-                    toks: res.data
-                })
-            })
-            .catch(err => console.log(err))
+        // const baseUrl = 'https://us-central1-tokker.cloudfunctions.net/api'
+        // axios.get('https://us-central1-tokker.cloudfunctions.net/api/toks')
+        //     .then(res => {
+        //         this.setState({
+        //             toks: res.data
+        //         })
+        //     })
+        //     .catch(err => console.log(err))
+
+        this.setState({
+            toks: 
+                [
+                {
+                    "tokId": "MgyFLw0U5otU1F83ZOCy",
+                    "body": "Hello hello hello",
+                    "userHandle": "user3",
+                    "createdAt": "2019-08-11T19:13:07.990Z",
+                    "commentCount": 0,
+                    "likeCount": 0,
+                    "userImage": "https://firebasestorage.googleapis.com/v0/b/tokker.appspot.com/o/no-img.png?alt=media"
+                },
+                {
+                    "tokId": "MgyFLw0U5otU1F83ZOCy",
+                    "body": "Hello hello hello",
+                    "userHandle": "user3",
+                    "createdAt": "2019-08-11T19:13:07.990Z",
+                    "commentCount": 0,
+                    "likeCount": 0,
+                    "userImage": "https://firebasestorage.googleapis.com/v0/b/tokker.appspot.com/o/no-img.png?alt=media"
+                },
+            ] 
+        })
     }
 
     render() {
         const { toks } = this.state
 
         let recentToksMarkup = toks  ? (
-        toks.map(tok => <Tok tok={tok} />)
+        toks.map(tok => <Tok key={tok.tokId} tok={tok} />)
         ) : <p>Loading...</p>
 
         return (
